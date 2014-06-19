b3f3b1d67cab654d479076e5e19c584f6fda0f4b
Rafael Macedo <macedo.rafaelfernandes@gmail.com>
2014-06-18 23:54:54 -0300

    remove old file

--- public/application.js
+++
@@ -1,7 +0,0 @@
-(function($, document, window, undefined) {
-  var websocket = io();
-
-  websocket.on('data', function(data) {
-    $("pre").append(String.fromCharCode.apply(null, new Uint8Array(data)));
-  });
-})($, document, window);
